3bf6c8a7b50828c05a3406e7996046b9a17a8158
Joaquim Neto <joaquimmy@gmail.com>
2023-07-29 02:42:57 -0300

    feat(css): put theme into variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import App from './App.tsx';
+
+import './theme.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
